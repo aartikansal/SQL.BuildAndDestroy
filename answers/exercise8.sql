d4e68a711ea63fc6cba62ef7fea57036a49481e2
aartikansal <aartilumb@gmail.com>
2020-03-14 17:11:09 -0400

    excercise 8 dome

---
+++ answers/exercise8.sql
@@ -0,0 +1,13 @@
+
+USE myNEWDATABASE;
+CREATE TABLE users(
+UserID INT,
+LastName VARCHAR(255),
+FirstName VARCHAR(255),
+Address VARCHAR(255),
+City VARCHAR(255)
+);
+SHOW TABLES;
+INSERT INTO users( userid, lastname, firstname, address, city)
+VALUES (200,'Aarti', 'Kansal','Phoenixville', 'Phoenixville');
+SELECT * FROM users
